aebe94aa02bcbc441d6da068dbef04ef148ebda5
Brian Tong <brktong@gmail.com>
2017-02-15 00:47:00 -0500

    generate texture from maps

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Framework from './framework'
 import Builder from './builder.js'
 import OBJLoader from './OBJLoader.js'
 import Turtle from './turtle.js'
+import CTurtle from './cturtle.js'
 import Lsystem, {LinkedListToString} from './lsystem.js'
 
 var settings = {
@@ -11,22 +12,26 @@ var settings = {
     resetCamera: function() {},
     newSeed: function(newVal) { settings.seed = Math.random(); },
     size: 10.0,
-    resolution: 128,
+    resolution: 64,
     split: 0.0
 }
 
 var lsystem_settings = {
-    Axiom: "[[[RA]RA]RA]",
+    Axiom: "A",
     Rules: [
-        {Rule: "A=RMA", Prob: 0.5},
-        {Rule: "R=-R", Prob: 1.0},
-        {Rule: "R=+R", Prob: 1.0},
-        {Rule: "R=*R", Prob: 1.0},
-        {Rule: "R=/R", Prob: 1.0},
-        {Rule: "R=MMR", Prob: 10.0},
-        {Rule: "R=M", Prob: 0.5},
+        {Rule: "A=[[[RRRM]RRRM]RRRM]", Prob: 1.0},
+        {Rule: "B=RMB", Prob: 1.0},
+        {Rule: "C=MC", Prob: 5.0},
+        {Rule: "C=M", Prob: 1.0},
+        {Rule: "R=-MR", Prob: 1.0},
+        {Rule: "R=+MR", Prob: 1.0},
+        {Rule: "R=*MR", Prob: 1.0},
+        {Rule: "R=/MR", Prob: 1.0},
+        {Rule: "P=*MR", Prob: 1.0},
+        {Rule: "P=/MR", Prob: 1.0},
+        {Rule: "M=[RB]PMC", Prob: 1.0},
     ],
-    iterations: 6,
+    iterations: 4,
     Render: function() {}
 }
 
@@ -42,7 +47,8 @@ var ZONES = {
 
 
 var city = {
-    grid: []
+    grid: [],
+    maps: {}
 }
 
 // called after the scene loads
@@ -75,81 +81,106 @@ function onLoad(framework) {
   gui.add(settings, 'split', 0, 10);
 
 
-  var f2 = gui.addFolder('Roads');
-  f2.add(lsystem_settings, 'Axiom')
+  var roads = gui.addFolder('Roads');
+  roads.add(lsystem_settings, 'Axiom')
   for (var i = 0; i < lsystem_settings.Rules.length; i++) {
-      f2.add(lsystem_settings.Rules[i], 'Rule');
-      f2.add(lsystem_settings.Rules[i], 'Prob');
+      roads.add(lsystem_settings.Rules[i], 'Rule');
+      roads.add(lsystem_settings.Rules[i], 'Prob');
   }
-
-
-  f2.add(lsystem_settings, 'Render').onChange(function() {
+  roads.add(lsystem_settings, 'Render').onChange(function() {
       regenerateCity(framework);
   });
-  f2.open();
+  roads.open();
 
   regenerateCity(framework);
 }
 
-
-function draw(framework) {
-
-}
-
-function generateTexture() {
-  var w = city.grid.length - 1;
-  var h = city.grid[0].length - 1;
-  // build a small canvas 32x64 and paint it in white
-  var canvas  = document.createElement('canvas');
-  canvas.width = w;
-  canvas.height = h;
-  var context = canvas.getContext('2d');
-  // plain it in white
-  context.fillStyle    = '#ffffff';
-  context.fillRect( 0, 0, w, h );
-  // draw the window rows - with a small noise to simulate light variations in each room
-
-  for (var i = 0; i < w; i++) {
-      for (var j = 0; j < h; j++) {
-          var x = Math.floor(city.grid.length * i / w);
-          var y = Math.floor(city.grid[0].length * j / h);
-          if (city.grid[x][h-y].zone == ZONES.ROAD.value) {
-              context.beginPath();
-              context.arc(i,j,1,0,2*Math.PI,false);
-              context.fillStyle = '#333';
-              context.fill();
-        } else {
-            var r = Math.floor(50 * Math.random()) + 100;
-            var g = Math.floor(50 * Math.random()) + 150;
-            var b = Math.floor(50 * Math.random());
-            context.fillStyle = 'rgb(' + [r, g, b].join( ',' )  + ')';
-            context.fillRect( i, j, 1, 1 );
-        }
-      }
-  }
-
-  // build a bigger canvas and copy the small one in it
-  // This is a trick to upscale the texture without filtering
-  var canvas2 = document.createElement( 'canvas' );
+function resizeCanvas(canvas) {
+  var canvas2 = document.createElement('canvas');
   canvas2.width    = 512;
   canvas2.height   = 512;
   var context = canvas2.getContext( '2d' );
-  // disable smoothing
   context.imageSmoothingEnabled        = false;
   context.webkitImageSmoothingEnabled  = false;
   context.mozImageSmoothingEnabled     = false;
-  // then draw the image
   context.drawImage( canvas, 0, 0, canvas2.width, canvas2.height );
-  // return the just built canvas2
-
-  var debug = document.getElementById("debug");
-  debug.appendChild(canvas2);
-
   return canvas2;
 }
 
-function loadMap(key, path) {
+function getMapValue(canvas, ni, nj) {
+  var i = canvas.width * ni;
+  var j = canvas.height * nj;
+  var context = canvas.getContext('2d');
+  var pixel = context.getImageData(i, j, 1, 1);
+  var data = pixel.data;
+  return new THREE.Vector4(data[0], data[1], data[2], data[3]/255);
+}
 
+function generateTexture() {
+  var size = settings.resolution * 3;
+  var canvas = document.createElement('canvas');
+  canvas.width = size;
+  canvas.height = size;
+  var context = canvas.getContext('2d');
+  context.fillStyle = '#ffffff';
+  context.fillRect(0, 0, size, size);
+
+  // generate grass texture
+  for (var i = 0; i < size; i++) {
+    for (var j = 0; j < size; j++) {
+      var r = Math.floor(50 * Math.random()) + 100;
+      var g = Math.floor(50 * Math.random()) + 150;
+      var b = Math.floor(50 * Math.random());
+      context.fillStyle = 'rgb(' + [r, g, b].join( ',' )  + ')';
+      context.fillRect( i, j, 1, 1 );
+    }
+  }
+
+  // generate roads
+  for (var i = 0; i < size; i++) {
+    for (var j = 0; j < size; j++) {
+      var norm_i = i / size;
+      var norm_j = j / size;
+      var rgba = getMapValue(city.maps['roads'], norm_i, norm_j);
+      if (rgba.x < 250) {
+        context.fillStyle = 'rgba(' + [30, 30, 30, (255 - rgba.x) / 255].join( ',' )  + ')';
+        context.fillRect( i, j, 1, 1 );
+      }
+    }
+  }
+  return resizeCanvas(canvas);
+}
+
+function loadMapFromCanvas(framework, key, canvas) {
+  var scene = framework.scene;
+  var renderer = framework.renderer;
+  var texture = new THREE.Texture(canvas);
+  texture.anisotropy = renderer.getMaxAnisotropy();
+  texture.needsUpdate = true;
+  var material  = new THREE.MeshBasicMaterial({
+    map: texture,
+    side: THREE.DoubleSide
+  });
+  var geometry = new THREE.PlaneBufferGeometry( settings.size, settings.size, 1 );
+  var plane = new THREE.Mesh( geometry, material );
+  plane.rotateX(Math.PI / 2.0);
+  plane.name = key;
+  scene.add(plane);
+  city.maps[key] = canvas;
+  console.log(city.maps);
+}
+
+function loadMapFromPath(framework, key, path) {
+  var img = new Image();
+  img.onload = function () {
+    var canvas  = document.createElement('canvas');
+    canvas.width = 512;
+    canvas.height = 512;
+    var context = canvas.getContext('2d');
+    context.drawImage(img, 0, 0);
+    loadMapFromCanvas(framework, key, canvas);
+  };
+  img.src = path;
 }
 
 function getZoneProperties(zoneValue) {
@@ -166,6 +197,9 @@ function regenerateCity(framework) {
     var renderer = framework.renderer;
     var gui = framework.gui;
     var stats = framework.stats;
+
+
+
 
     // clear scene
     scene.children.forEach(function(object){
@@ -186,97 +220,66 @@ function regenerateCity(framework) {
     var lsys = new Lsystem(lsystem_settings.Axiom);
     lsys.UpdateRules(lsystem_settings.Rules);
     var result = lsys.DoIterations(lsystem_settings.iterations);
-    turtle = new Turtle(city.grid);
-    turtle.renderSymbols(result);
+    // turtle = new Turtle(city.grid);
+    // turtle.renderSymbols(result);
 
-    generateRoads();
+    var cturtle = new CTurtle(256,256);
+    var turtle_canvas = cturtle.renderSymbols(result);
+    loadMapFromCanvas(framework, 'roads', turtle_canvas);
+    var debug = document.getElementById('debug');
+    debug.appendChild(turtle_canvas);
 
-    var texture = new THREE.Texture(generateTexture());
-    texture.anisotropy = renderer.getMaxAnisotropy();
-    texture.needsUpdate = true;
+    loadMapFromPath(framework, 'population', './maps/population.png');
 
-    // build the mesh
-    var material  = new THREE.MeshLambertMaterial({
-      map: texture,
-      side: THREE.DoubleSide
-    });
+    // HACK: wait for resources to load
+    setTimeout(function() {
 
-    // redraw planes
-    var geometry = new THREE.PlaneBufferGeometry( settings.size, settings.size, 1 );
-    // var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-    var plane = new THREE.Mesh( geometry, material );
-    plane.rotateX(Math.PI / 2.0);
-    plane.name = "plane_terrain";
-    scene.add( plane );
+          var texture = new THREE.Texture(generateTexture());
+          texture.anisotropy = renderer.getMaxAnisotropy();
+          texture.needsUpdate = true;
 
-    var geometry = new THREE.PlaneBufferGeometry( settings.size, settings.size, 1 );
-    var material = new THREE.MeshLambertMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-    var plane = new THREE.Mesh( geometry, material );
-    plane.rotateX(Math.PI / 2.0);
-    plane.name = "plane_popDensity";
-    scene.add( plane );
+          // build the mesh
+          var material  = new THREE.MeshBasicMaterial({
+            map: texture,
+            side: THREE.DoubleSide
+          });
 
-    var geometry = new THREE.PlaneBufferGeometry( settings.size, settings.size, 1 );
-    var material = new THREE.MeshLambertMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-    var plane = new THREE.Mesh( geometry, material );
-    plane.rotateX(Math.PI / 2.0);
-    plane.name = "plane_landValue";
-    scene.add( plane );
+          // redraw planes
+          var geometry = new THREE.PlaneBufferGeometry( settings.size, settings.size, 1 );
+          // var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+          var plane = new THREE.Mesh( geometry, material );
+          plane.rotateX(Math.PI / 2.0);
+          plane.name = "plane_terrain";
+          scene.add( plane );
 
+          generateBuildings(framework);
+          var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+          scene.add( light );
+          var light = new THREE.PointLight( 0xffffff, 10, 100 );
+          light.position.set( 50, 50, 50 );
+          scene.add( light );
 
-    generateBuildings(framework);
-    var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
-    scene.add( light );
-    var light = new THREE.PointLight( 0xffffff, 10, 100 );
-    light.position.set( 50, 50, 50 );
-    scene.add( light );
-}
-
-function generateRoads() {
-
-    // vertical streets
-    for (var i = 0; i < settings.resolution; i+=16) {
-        for (var j = 0; j < settings.resolution; j++) {
-            city.grid[i][j] = { zone: ZONES.ROAD.value };
-        }
-    }
-
-    // horizontal streets
-    for (var i = 0; i < settings.resolution; i+=8) {
-        for (var j = 0; j < settings.resolution; j++) {
-            city.grid[j][i] = { zone: ZONES.ROAD.value };
-        }
-    }
-}
-
-// http://stackoverflow.com/questions/934012/get-image-data-in-javascript
-function getBase64FromImageUrl(url) {
-    var img = new Image();
-
-    img.setAttribute('crossOrigin', 'anonymous');
-
-    img.onload = function () {
-        var canvas = document.createElement("canvas");
-        canvas.width =this.width;
-        canvas.height =this.height;
-
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(this, 0, 0);
-
-        var dataURL = canvas.toDataURL("image/png");
-
-        // alert(dataURL.replace(/^data:image\/(png|jpg);base64,/, ""));
-
-
-
-        var imgd = ctx.getImageData(10, 11, 512, 512);
-        var pix = imgd.data;
-        console.log(pix.length);
-    };
-
-    img.src = url;
+    }, 3000);
 
 }
+
+// function generateRoads() {
+//
+//     // vertical streets
+//     for (var i = 0; i < settings.resolution; i+=16) {
+//         for (var j = 0; j < settings.resolution; j++) {
+//             city.grid[i][j] = { zone: ZONES.ROAD.value };
+//         }
+//     }
+//
+//     // horizontal streets
+//     for (var i = 0; i < settings.resolution; i+=8) {
+//         for (var j = 0; j < settings.resolution; j++) {
+//             city.grid[j][i] = { zone: ZONES.ROAD.value };
+//         }
+//     }
+// }
+
 
 function generateBuildings(framework) {
     var scene = framework.scene;
@@ -286,33 +289,46 @@ function generateBuildings(framework) {
     var offset = settings.size / 2.0 - square_size / 2.0;
     var builder = new Builder();
 
+    var roads = city.maps['roads'];
+
+
     for (var i = 0; i < settings.resolution; i++) {
         for (var j = 0; j < settings.resolution; j++) {
 
 
-            if (city.grid[i][j].zone == ZONES.UNZONED.value) {
-                if (Math.random() < 0.5) {
-                    continue;
-                }
-                var options = {
-                    zone: city.grid[i][j].zone,
-                    length: settings.size / settings.resolution,
-                    width: settings.size / settings.resolution,
-                    height: settings.size / settings.resolution
-                };
-                var building = builder.generateBuilding(options);
-                building.position.set(i * square_size - offset, 0.05, j * square_size - offset);
-                scene.add(building);
-            } else if (city.grid[i][j].zone == ZONES.ROAD.value) {
-                // var grid_color = getZoneProperties(city.grid[i][j].zone).color;
-                // var geometry = new THREE.PlaneBufferGeometry( square_size, square_size, 1 );
-                // // var material = new THREE.MeshBasicMaterial( {color: grid_color} );
-                // var plane = new THREE.Mesh( geometry, road_material );
-                // plane.rotateX(Math.PI / 2.0);
-                // plane.position.set(i * square_size - offset, 0.01, j * square_size - offset);
-                // plane.name = "square";
-                // scene.add( plane );
+          var isClear = true;
+          for (var k = -1; k < 2; k++) {
+            for (var m = -1; m < 2; m++) {
+              var norm_i = (i + k) / settings.resolution;
+              var norm_j = (settings.resolution - (j + m)) / settings.resolution;
+              var rgba = getMapValue(roads, norm_i, norm_j);
+              if (rgba.x != 255) {
+                isClear = false;
+                break;
+              }
             }
+          }
+
+          if (isClear) {
+            if (Math.random() < 0.3) {
+                continue;
+            }
+
+            var norm_i = i / settings.resolution;
+            var norm_j = (settings.resolution - j) / settings.resolution;
+            var rgba = getMapValue(city.maps['population'], norm_i, norm_j);
+
+            var density = Math.pow((255 - rgba.x) / 255, 2) * 20;
+            var options = {
+                zone: city.grid[i][j].zone,
+                length: settings.size / settings.resolution,
+                width: settings.size / settings.resolution,
+                height: density * settings.size / settings.resolution * Math.random()
+            };
+            var building = builder.generateBuilding(options);
+            building.position.set(i * square_size - offset, 0, j * square_size - offset);
+            scene.add(building);
+          }
         }
     }
 
@@ -330,24 +346,12 @@ function onUpdate(framework) {
     var gui = framework.gui;
     var stats = framework.stats;
 
-    var plane_terrain = scene.getObjectByName("plane_terrain");
-    if (plane_terrain !== undefined) {
-        plane_terrain.geometry = new THREE.PlaneBufferGeometry( settings.size, settings.size, 1 );
+    var i = 1;
+    for (var key in city.maps) {
+      var plane = scene.getObjectByName(key);
+      plane.position.set(0, -i * settings.split, 0);
+      i++;
     }
-
-    var plane_popDensity = scene.getObjectByName("plane_popDensity");
-    if (plane_popDensity !== undefined) {
-        plane_popDensity.geometry = new THREE.PlaneBufferGeometry( settings.size, settings.size, 1 );
-        plane_popDensity.position.set(0, -settings.split, 0);
-    }
-
-    var plane_landValue = scene.getObjectByName("plane_landValue");
-    if (plane_landValue !== undefined) {
-        plane_landValue.geometry = new THREE.PlaneBufferGeometry( settings.size, settings.size, 1 );
-        plane_landValue.position.set(0, -2 * settings.split, 0);
-    }
-
-
 }
 
 // when the scene is done initializing, it will call onLoad, then on frame updates, call onUpdate
