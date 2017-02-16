e5ee33a91a7b86a70ca0d0ff4d8a64ae2c4d810d
Eric Chiu <ericchiu@huntsman-ve565-1473.apn.wlan.upenn.edu>
2017-02-15 22:31:02 -0500

    finished building generation

--- src/lsystem.js
+++ src/lsystem.js
@@ -12,45 +12,58 @@ function Rule(prob, str) {
 export class Shape {
 	constructor(symbol) {
     	this.symbol = symbol;
-	    this.position = new THREE.Vector3(0, 0, 0); //WARNING: position is at the BOTTOM of the shape
+	    this.position = new THREE.Vector3(0, 0, 0); //WARNING: position is at BOTTOM CENTER of shape, as defined in OBJ files
 	    this.rotation = new THREE.Vector3(0, 0, 0);
 	    this.scale = new THREE.Vector3(1, 1, 1); //scale.y is the height of shape
 	    this.terminal = false;
+	    this.geom_type = 'Unknown';
 	}
 };
 
 //axiom will be a set of shapes
 export default class Lsystem {
 
-	constructor(shapes, iterations) {
+	constructor(shapes) {
 		this.shapes = new Set();
 		this.grammar = {};
-		this.grammar['X'] = [
+
+		//apartment rules
+		this.grammar['A'] = [
 			new Rule(1.0, 'bmr')
 		];
-		/*
 		this.grammar['M'] = [
 			new Rule(1.0, 'f')
 		];
-		*/
-		this.iterations = 0;
+		this.grammar['F'] = [
+			new Rule(1.0, 'w')
+		];
+		this.grammar['B'] = [
+			new Rule(1.0, 'w')
+		];
+		this.grammar['R'] = [
+			new Rule(1.0, 'w')
+		];
+
+		//skyscraper rules
+		this.grammar['S'] = [
+			new Rule(1.0, 'ht')
+		];
 
 		//binds grammar to method stored in this class
 	    this.renderGrammar = {
 	        'b' : this.makeBase.bind(this),
 	        'm' : this.makeMiddle.bind(this),
-	        'r' : this.makeRoof.bind(this)
-	        //'f' : this.makeFloors.bind(this)
+	        'r' : this.makeRoof.bind(this),
+	        'f' : this.makeFloors.bind(this),
+	        'w' : this.makeWindows.bind(this),
+
+	        'h' : this.makeSkyscraperBase.bind(this),
+	        't' : this.makeSkyscraperRoof.bind(this)
 	    };
 	    
 		// Set up the axiom shapes
 		if (typeof shapes !== "undefined") {
 			this.shapes = shapes;
-		}
-		// Set up iterations (the number of times you 
-		// should expand the axiom in DoIterations)
-		if (typeof iterations !== "undefined") {
-			this.iterations = iterations;
 		}
 	};
 
@@ -68,21 +81,18 @@ export default class Lsystem {
 	doIterations(n) {	
 
 		var finalShapes = this.shapes;
-		console.log(finalShapes);
 
 		for (var count = 0; count < n; count++) {
 
 			var tempShapes = new Set();
-			console.log("count");
+
 			//iterate through the current shapes
 			for(var shape of finalShapes.values()) {
-				console.log("inside this");
 				//if shape is terminal, do not replace using grammar, just add to new set
 				if (shape.terminal) {
 					tempShapes.add(shape);
 				}
 				else {
-					console.log('iterating');
 					//iterate through the grammar to find matching symbol
 					for (var key in this.grammar) {
 						if (shape.symbol === key) {
@@ -116,7 +126,6 @@ export default class Lsystem {
 	        finalShapes = tempShapes;
     	}
 
-    	console.log(finalShapes);
 		return finalShapes;
 	};
 
@@ -125,16 +134,8 @@ export default class Lsystem {
 		shape.position = new THREE.Vector3(replacedShape.position.x, replacedShape.position.y, replacedShape.position.z);
 		shape.rotation = new THREE.Vector3(replacedShape.rotation.x, replacedShape.rotation.y, replacedShape.rotation.z);
 		shape.scale = new THREE.Vector3(replacedShape.scale.x, 1, replacedShape.scale.z);
-		shape.terminal = true;
-		shapeSet.add(shape);
-	};
-
-	makeMiddle(shapeSet, replacedShape) {
-		var shape = new Shape('M');
-		shape.position = new THREE.Vector3(replacedShape.position.x, replacedShape.position.y + 1.0, replacedShape.position.z);
-		shape.rotation = new THREE.Vector3(replacedShape.rotation.x, replacedShape.rotation.y, replacedShape.rotation.z);
-		shape.scale = new THREE.Vector3(replacedShape.scale.x, replacedShape.scale.y - 2.0, replacedShape.scale.z);
-		shape.terminal = true;
+		shape.terminal = false;
+		shape.geom_type = 'ApartmentBase';
 		shapeSet.add(shape);
 	};
 
@@ -143,14 +144,263 @@ export default class Lsystem {
 		shape.position = new THREE.Vector3(replacedShape.position.x, replacedShape.position.y + replacedShape.scale.y - 1.0, replacedShape.position.z);
 		shape.rotation = new THREE.Vector3(replacedShape.rotation.x, replacedShape.rotation.y, replacedShape.rotation.z);
 		shape.scale = new THREE.Vector3(replacedShape.scale.x, 1, replacedShape.scale.z);
-		shape.terminal = true;
+		shape.terminal = false;
+		shape.geom_type = 'ApartmentRoof';
 		shapeSet.add(shape);
 	};
 
-	/*
-	makeFloors(shapeSet, replacedShape) {
-
+	makeMiddle(shapeSet, replacedShape) {
+		var shape = new Shape('M');
+		shape.position = new THREE.Vector3(replacedShape.position.x, replacedShape.position.y + 2.0, replacedShape.position.z);
+		shape.rotation = new THREE.Vector3(replacedShape.rotation.x, replacedShape.rotation.y, replacedShape.rotation.z);
+		shape.scale = new THREE.Vector3(replacedShape.scale.x, replacedShape.scale.y - 3.0, replacedShape.scale.z);
+		shape.terminal = false;
+		shape.geom_type = 'ApartmentMiddle';
+		shapeSet.add(shape);
 	};
-	*/
+
+	makeFloors(shapeSet, replacedShape) {
+		var sumFloorHeight = 0;
+		while (sumFloorHeight < replacedShape.scale.y) {
+			var floorHeight = Math.round(1.0 + Math.random());
+			if (sumFloorHeight + floorHeight >  replacedShape.scale.y) {
+				floorHeight = 1;
+			}
+			var shape = new Shape('F');
+			shape.position = new THREE.Vector3(replacedShape.position.x, replacedShape.position.y + sumFloorHeight, replacedShape.position.z);
+			shape.rotation = new THREE.Vector3(replacedShape.rotation.x, replacedShape.rotation.y, replacedShape.rotation.z);
+			shape.scale = new THREE.Vector3(replacedShape.scale.x, floorHeight, replacedShape.scale.z);
+			shape.terminal = false;
+			if (floorHeight == 1) {
+				shape.geom_type = 'ApartmentFloor1';
+			}
+			else {
+				shape.geom_type = 'ApartmentFloor2';
+			}
+			shapeSet.add(shape);
+			sumFloorHeight += floorHeight;
+		}
+	};
+
+	makeWindows(shapeSet, replacedShape) {
+
+		//determine the type of windows to make
+		var side_type, corner_type;
+		if (replacedShape.geom_type === 'ApartmentFloor1') {
+			side_type = 'ApartmentFloorSide1';
+			corner_type = 'ApartmentFloorCorner1';
+		}
+		else if (replacedShape.geom_type === 'ApartmentFloor2'){
+			side_type = 'ApartmentFloorSide2';
+			corner_type = 'ApartmentFloorCorner2';
+		}
+		else if (replacedShape.geom_type === 'ApartmentBase') {
+			side_type = 'ApartmentBaseSide';
+			corner_type = 'ApartmentBaseCorner';
+		}
+		else {
+			side_type = 'ApartmentRoofSide';
+			corner_type = 'ApartmentRoofCorner';
+		}
+
+		//boolean to see whether shape created is corner or side
+		var isCorner = true;
+
+		//front side of floor
+		var xPos = replacedShape.position.x + (replacedShape.scale.x/2.0) - 0.5;
+		var zPos = replacedShape.position.z + (replacedShape.scale.z/2.0) - 0.5;
+		var endXPos = replacedShape.position.x - (replacedShape.scale.x/2.0) + 0.5;
+		while (xPos > endXPos) {
+			var shape = new Shape('W');
+			shape.position = new THREE.Vector3(xPos, replacedShape.position.y, zPos);
+			shape.rotation = new THREE.Vector3(replacedShape.rotation.x, replacedShape.rotation.y, replacedShape.rotation.z);
+			shape.scale = new THREE.Vector3(1.0, 1.0, 1.0);
+			shape.terminal = true;
+			if (isCorner) {
+				shape.geom_type = corner_type;
+				isCorner = false;
+			}
+			else {
+				shape.geom_type = side_type;
+			}
+			shapeSet.add(shape);
+			xPos -= 1;
+		}
+		isCorner = true;
+
+		//right side of floor (if positive z axis is facing toward you)
+		xPos = replacedShape.position.x + (replacedShape.scale.x/2.0) - 0.5;
+		zPos = replacedShape.position.z - (replacedShape.scale.z/2.0) + 0.5;
+		var endZPos = replacedShape.position.z + (replacedShape.scale.z/2.0) - 0.5;
+		while (zPos < endZPos) {
+			var shape = new Shape('W');
+			shape.position = new THREE.Vector3(xPos, replacedShape.position.y, zPos);
+			shape.rotation = new THREE.Vector3(replacedShape.rotation.x, replacedShape.rotation.y + 90.0, replacedShape.rotation.z);
+			shape.scale = new THREE.Vector3(1.0, 1.0, 1.0);
+			shape.terminal = true;
+			if (isCorner) {
+				shape.geom_type = corner_type;
+				isCorner = false;
+			}
+			else {
+				shape.geom_type = side_type;
+			}
+			shapeSet.add(shape);
+			zPos += 1;
+		}
+		isCorner = true;
+
+		//back side of floor
+		xPos = replacedShape.position.x - (replacedShape.scale.x/2.0) + 0.5;
+		zPos = replacedShape.position.z - (replacedShape.scale.z/2.0) + 0.5;
+		endXPos = replacedShape.position.x + (replacedShape.scale.x/2.0) - 0.5;
+		while (xPos < endXPos) {
+			var shape = new Shape('W');
+			shape.position = new THREE.Vector3(xPos, replacedShape.position.y, zPos);
+			shape.rotation = new THREE.Vector3(replacedShape.rotation.x, replacedShape.rotation.y + 180.0, replacedShape.rotation.z);
+			shape.scale = new THREE.Vector3(1.0, 1.0, 1.0);
+			shape.terminal = true;
+			if (isCorner) {
+				shape.geom_type = corner_type;
+				isCorner = false;
+			}
+			else {
+				shape.geom_type = side_type;
+			}
+			shapeSet.add(shape);
+			xPos += 1;
+		}
+		isCorner = true;
+
+		//left side of floor
+		xPos = replacedShape.position.x - (replacedShape.scale.x/2.0) + 0.5;
+		zPos = replacedShape.position.z + (replacedShape.scale.z/2.0) - 0.5;
+		endZPos = replacedShape.position.z - (replacedShape.scale.z/2.0) + 0.5;
+		while (zPos > endZPos) {
+			var shape = new Shape('W');
+			shape.position = new THREE.Vector3(xPos, replacedShape.position.y, zPos);
+			shape.rotation = new THREE.Vector3(replacedShape.rotation.x, replacedShape.rotation.y + 270.0, replacedShape.rotation.z);
+			shape.scale = new THREE.Vector3(1.0, 1.0, 1.0);
+			shape.terminal = true;
+			if (isCorner) {
+				shape.geom_type = corner_type;
+				isCorner = false;
+			}
+			else {
+				shape.geom_type = side_type;
+			}
+			shapeSet.add(shape);
+			zPos -= 1;
+		}
+		isCorner = true;
+	}
+
+	makeSkyscraperBase(shapeSet, replacedShape) {
+
+		//determine the type of windows to make
+		var side_type = 'SkyscraperSide';
+		var corner_type = 'SkyscraperCorner';
+
+		//boolean to see whether shape created is corner or side
+		var isCorner = true;
+
+		//front side of floor
+		var xPos = replacedShape.position.x + (replacedShape.scale.x/2.0) - 0.5;
+		var zPos = replacedShape.position.z + (replacedShape.scale.z/2.0) - 0.5;
+		var endXPos = replacedShape.position.x - (replacedShape.scale.x/2.0) + 0.5;
+		while (xPos > endXPos) {
+			var shape = new Shape('H');
+			shape.position = new THREE.Vector3(xPos, replacedShape.position.y, zPos);
+			shape.rotation = new THREE.Vector3(replacedShape.rotation.x, replacedShape.rotation.y, replacedShape.rotation.z);
+			shape.scale = new THREE.Vector3(1.0, replacedShape.scale.y - 1.0, 1.0);
+			shape.terminal = true;
+			if (isCorner) {
+				shape.geom_type = corner_type;
+				isCorner = false;
+			}
+			else {
+				shape.geom_type = side_type;
+			}
+			shapeSet.add(shape);
+			xPos -= 1;
+		}
+		isCorner = true;
+
+		//right side of floor (if positive z axis is facing toward you)
+		xPos = replacedShape.position.x + (replacedShape.scale.x/2.0) - 0.5;
+		zPos = replacedShape.position.z - (replacedShape.scale.z/2.0) + 0.5;
+		var endZPos = replacedShape.position.z + (replacedShape.scale.z/2.0) - 0.5;
+		while (zPos < endZPos) {
+			var shape = new Shape('H');
+			shape.position = new THREE.Vector3(xPos, replacedShape.position.y, zPos);
+			shape.rotation = new THREE.Vector3(replacedShape.rotation.x, replacedShape.rotation.y + 90.0, replacedShape.rotation.z);
+			shape.scale = new THREE.Vector3(1.0, replacedShape.scale.y - 1.0, 1.0);
+			shape.terminal = true;
+			if (isCorner) {
+				shape.geom_type = corner_type;
+				isCorner = false;
+			}
+			else {
+				shape.geom_type = side_type;
+			}
+			shapeSet.add(shape);
+			zPos += 1;
+		}
+		isCorner = true;
+
+		//back side of floor
+		xPos = replacedShape.position.x - (replacedShape.scale.x/2.0) + 0.5;
+		zPos = replacedShape.position.z - (replacedShape.scale.z/2.0) + 0.5;
+		endXPos = replacedShape.position.x + (replacedShape.scale.x/2.0) - 0.5;
+		while (xPos < endXPos) {
+			var shape = new Shape('H');
+			shape.position = new THREE.Vector3(xPos, replacedShape.position.y, zPos);
+			shape.rotation = new THREE.Vector3(replacedShape.rotation.x, replacedShape.rotation.y + 180.0, replacedShape.rotation.z);
+			shape.scale = new THREE.Vector3(1.0, replacedShape.scale.y - 1.0, 1.0);
+			shape.terminal = true;
+			if (isCorner) {
+				shape.geom_type = corner_type;
+				isCorner = false;
+			}
+			else {
+				shape.geom_type = side_type;
+			}
+			shapeSet.add(shape);
+			xPos += 1;
+		}
+		isCorner = true;
+
+		//left side of floor
+		xPos = replacedShape.position.x - (replacedShape.scale.x/2.0) + 0.5;
+		zPos = replacedShape.position.z + (replacedShape.scale.z/2.0) - 0.5;
+		endZPos = replacedShape.position.z - (replacedShape.scale.z/2.0) + 0.5;
+		while (zPos > endZPos) {
+			var shape = new Shape('H');
+			shape.position = new THREE.Vector3(xPos, replacedShape.position.y, zPos);
+			shape.rotation = new THREE.Vector3(replacedShape.rotation.x, replacedShape.rotation.y + 270.0, replacedShape.rotation.z);
+			shape.scale = new THREE.Vector3(1.0, replacedShape.scale.y - 1.0, 1.0);
+			shape.terminal = true;
+			if (isCorner) {
+				shape.geom_type = corner_type;
+				isCorner = false;
+			}
+			else {
+				shape.geom_type = side_type;
+			}
+			shapeSet.add(shape);
+			zPos -= 1;
+		}
+		isCorner = true;
+	}
+
+	makeSkyscraperRoof(shapeSet, replacedShape) {
+		var shape = new Shape('T');
+		shape.position = new THREE.Vector3(replacedShape.position.x, replacedShape.position.y + replacedShape.scale.y - 1.0, replacedShape.position.z);
+		shape.rotation = new THREE.Vector3(replacedShape.rotation.x, replacedShape.rotation.y, replacedShape.rotation.z);
+		shape.scale = new THREE.Vector3(replacedShape.scale.x, 1, replacedShape.scale.z);
+		shape.terminal = true;
+		shape.geom_type = 'SkyscraperRoof';
+		shapeSet.add(shape);
+	}
 
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 
-const THREE = require('three'); // older modules are imported like this. You shouldn't have to worry about this much
+const THREE = require('three') // older modules are imported like this. You shouldn't have to worry about this much
+const OBJLoader = require('three-obj-loader')(THREE)
 import Framework from './framework'
 import {Shape} from './lsystem.js'
 import Lsystem from './lsystem.js'
-//import Lsystem, {linkedListToString} from './lsystem.js'
-//import Turtle from './turtle.js'
 
-//var turtle;
+var shapeSet;
+//initialize obj loading before anything, or else multithreading causes issues
+var typeToObjMap = initializeMap();
 
 /*
 var Sliders = function() {
@@ -25,7 +26,7 @@ function onLoad(framework) {
 
   // initialize a simple box and material
   var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
-  directionalLight.color.set(0xFFDAB9);
+  directionalLight.color.set(0xffffff);
   //directionalLight.color.setHSL(0.1, 1, 0.95);
   directionalLight.position.set(1, 3, 2);
   directionalLight.position.multiplyScalar(10);
@@ -36,45 +37,20 @@ function onLoad(framework) {
   camera.lookAt(new THREE.Vector3(0,0,0));
 
   // initialize LSystem and a Turtle to draw
-  var shapeSet = new Set();
+  shapeSet = new Set();
 
-  var building = new Shape('X');
+  var building = new Shape('A');
   building.position = new THREE.Vector3(0, 0, 0);
   building.rotation = new THREE.Vector3(0, 0, 0);
-  building.scale = new THREE.Vector3(6, 8, 6);
+  building.scale = new THREE.Vector3(7, 15, 5);
+  building.geom_type = 'Apartment';
   shapeSet.add(building);
-  console.log(shapeSet.values());
-
   
-  var lsys = new Lsystem(shapeSet, 3);
+  var lsys = new Lsystem(shapeSet);
   shapeSet = lsys.doIterations(3);
-  //turtle = new Turtle(scene);
 
-
-  var singleGeometry = new THREE.Geometry();
-  for (var shape of shapeSet.values()) {
-    console.log("called");
-    var box = new THREE.BoxGeometry(1, 1, 1);
-    var boxMesh = new THREE.Mesh(box);
-
-    //apply scale
-    var mat4 = new THREE.Matrix4();
-    mat4.makeScale(shape.scale.x, shape.scale.y, shape.scale.z);
-    boxMesh.applyMatrix(mat4);
-
-    //apply rotation
-
-    //apply translation
-    var mat6 = new THREE.Matrix4();
-    mat6.makeTranslation(shape.position.x, shape.position.y + (shape.scale.y/2.0), shape.position.z);
-    boxMesh.applyMatrix(mat6);
-
-    singleGeometry.merge(boxMesh.geometry, boxMesh.matrix);
-  }
-  var material = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true});
-  var mesh = new THREE.Mesh(singleGeometry, material);
-  scene.add(mesh);
-
+  //parse the shape set and adds to scene
+  parseShapeSet(scene);
 
   gui.add(camera, 'fov', 0, 180).onChange(function(newVal) {
     camera.updateProjectionMatrix();
@@ -115,6 +91,84 @@ function doLsystem(lsystem, iterations, turtle, anglefactor) {
     turtle.renderSymbols(result);
 }
 */
+
+function initializeMap() {
+  var tempMap = new Map();
+  var objLoader = new THREE.OBJLoader();
+  objLoader.load('/geo/ApartmentBaseSide.obj', function(obj) { typeToObjMap.set('ApartmentBaseSide', obj.children[0].geometry) });
+  objLoader.load('/geo/ApartmentBaseCorner.obj', function(obj) { typeToObjMap.set('ApartmentBaseCorner', obj.children[0].geometry) });
+  objLoader.load('/geo/ApartmentFloorSide1.obj', function(obj) { tempMap.set('ApartmentFloorSide1', obj.children[0].geometry); });
+  objLoader.load('/geo/ApartmentFloorCorner1.obj', function(obj) { tempMap.set('ApartmentFloorCorner1', obj.children[0].geometry); });
+  objLoader.load('/geo/ApartmentFloorSide2.obj', function(obj) { tempMap.set('ApartmentFloorSide2', obj.children[0].geometry); });
+  objLoader.load('/geo/ApartmentFloorCorner2.obj', function(obj) { tempMap.set('ApartmentFloorCorner2', obj.children[0].geometry); });
+  objLoader.load('/geo/ApartmentRoofSide.obj', function(obj) { typeToObjMap.set('ApartmentRoofSide', obj.children[0].geometry) });
+  objLoader.load('/geo/ApartmentRoofCorner.obj', function(obj) { typeToObjMap.set('ApartmentRoofCorner', obj.children[0].geometry) });
+
+  objLoader.load('/geo/SkyscraperSide.obj', function(obj) { typeToObjMap.set('SkyscraperSide', obj.children[0].geometry) });
+  objLoader.load('/geo/SkyscraperCorner.obj', function(obj) { typeToObjMap.set('SkyscraperCorner', obj.children[0].geometry) });
+  objLoader.load('/geo/SkyscraperRoof.obj', function(obj) { typeToObjMap.set('SkyscraperRoof', obj.children[0].geometry) });
+
+  return tempMap;
+}
+
+function parseShapeSet(scene) {
+
+  var material = new THREE.MeshLambertMaterial({color: 0xffffff, wireframe: false});
+  //var singleGeometry = new THREE.Geometry();
+
+  for (var shape of shapeSet.values()) {
+
+    var box;
+    if (typeof typeToObjMap.get(shape.geom_type) !== "undefined") {
+      box = typeToObjMap.get(shape.geom_type);
+    }
+    else {
+      box = new THREE.BoxGeometry(1, 1, 1);
+    }
+    var boxMesh = new THREE.Mesh(box, material);
+
+    //apply scale
+    var mat4 = new THREE.Matrix4();
+    mat4.makeScale(shape.scale.x, shape.scale.y, shape.scale.z);
+    boxMesh.applyMatrix(mat4);
+
+    //apply rotation
+    if (shape.rotation.x != 0) {
+        var q = new THREE.Quaternion();
+        //forward rotation is not affected by angle factor, rotation about the trunk
+        q.setFromAxisAngle(new THREE.Vector3(1, 0, 0), shape.rotation.x * Math.PI/180.0);
+        var mat5 = new THREE.Matrix4();
+        mat5.makeRotationFromQuaternion(q);
+        boxMesh.applyMatrix(mat5);
+    }
+    if (shape.rotation.y != 0) {
+        var q = new THREE.Quaternion();
+        //up rotation is not affected by angle factor, main trunk curve
+        q.setFromAxisAngle(new THREE.Vector3(0, 1, 0), shape.rotation.y * Math.PI/180.0);
+        var mat5 = new THREE.Matrix4();
+        mat5.makeRotationFromQuaternion(q);
+        boxMesh.applyMatrix(mat5);
+    }
+    if (shape.rotation.z != 0) {
+        var q = new THREE.Quaternion();
+        q.setFromAxisAngle(new THREE.Vector3(0, 0, 1), shape.rotation.z * Math.PI/180.0);
+        var mat5 = new THREE.Matrix4();
+        mat5.makeRotationFromQuaternion(q);
+        boxMesh.applyMatrix(mat5);
+    }
+
+    //apply translation
+    var mat6 = new THREE.Matrix4();
+    mat6.makeTranslation(shape.position.x, shape.position.y, shape.position.z);
+    boxMesh.applyMatrix(mat6);
+    scene.add(boxMesh);
+    //singleGeometry.merge(boxMesh.geometry, boxMesh.matrix);
+  }
+  
+  //var mesh = new THREE.Mesh(singleGeometry, material);
+  //scene.add(mesh);
+
+}
 
 // called on frame updates
 function onUpdate(framework) {
