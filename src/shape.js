17585ff7ccff120f2fb44826f5589a6d0a57a101
eldu <eldu@seas.upenn.edu>
2017-02-17 22:00:12 -0500

    roofs

--- src/shape.js
+++ src/shape.js
@@ -30,8 +30,8 @@ export default class Shape {
 			// this.boundingBox = new THREE.BoxGeometry(10, 10, 10);
 			this.mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1),
 				lambertWhite);
-			this.mesh.scale.set(5, 5, 5);
-			this.mesh.position.set(0, 2.5, 0);
+			this.mesh.scale.set(10, 10, 10);
+			this.mesh.position.set(0, 5, 0);
 			this.iteration = 0;
 
 			// For continuation purposes
@@ -87,12 +87,17 @@ export default class Shape {
 		s_child.setComponent(1, s_axis);
 
 		for(var i = 0; i < numFloors; i++) {
-			var child = new THREE.Mesh(new THREE.BoxGeometry(s_child.x, s_child.y, s_child.z),
+			var child = new THREE.Mesh(new THREE.BoxGeometry(1.0, 1.0, 1.0),
 			new THREE.MeshLambertMaterial( {color: randColor()} ));
+			child.scale.set(s_child.x, s_child.y, s_child.z);
 
 			child.position.set(p.x, p.y - s.y / 2.0 + s_axis / 2.0 + s_axis * i, p.z);
-			var shape = new Shape(this, child, i.toString());
-			if (i == numFloors - 1) {
+			var shape = new Shape(this, child, 'floor');
+			
+			if (i == 0) {
+				shape.name = 'bottom';
+			}
+			else if (i == numFloors - 1) {
 				shape.name = 'top';
 			}
 			this.children.push(shape);
@@ -193,6 +198,105 @@ export default class Shape {
 		}
 	};
 
+		// Returns a (1, 1, 1) rectangular prism with the top shrunk in so that when scaled
+	// to the input it looks like a oning of a building in the forbidden city
+	createOningGeometry(scale, width) {
+	  if (!width) {
+	  	width = 1.0; // Standard width of the oning
+	  }
+
+	  var oning = new THREE.BoxGeometry( 1.0, 1.0, 1.0 );
+	  var v = oning.vertices; 
+
+	  // console.log(oning.vertices);
+	  var sx = (scale.x - width) / (scale.x * 2.0);
+	  var sz = (scale.z - width) / (scale.z * 2.0);
+
+	  v[0].set(sx, 0.5, sz);
+	  v[1].set(sx, 0.5, -sz);
+	  v[4].set(-sx, 0.5, -sz);
+	  v[5].set(-sx, 0.5, sz);
+	  var oning_mesh = new THREE.Mesh(oning, new THREE.MeshLambertMaterial());
+	  oning_mesh.scale.set(scale.x, scale.y, scale.z);
+	  return oning_mesh;
+	};
+
+	createRoofGeometry(scale, width) {
+		this.name = '';
+
+		if (!width) {
+			width = 2.0;
+		}
+
+		var roof = new THREE.BoxGeometry( 1.0, 1.0, 1.0 );
+		var v = roof.vertices;
+
+		var sx, sz;
+		if (scale.x <= scale.z) {
+			sx = (scale.x - width) / (scale.x * 2.0);
+			sz = 0.0;
+		} else { // scale.z > scale.x
+			sx = 0.0;
+			sz = (scale.z - width) / (scale.z * 2.0);
+		}
+
+		v[0].set(sx, 0.5, sz);
+		v[1].set(sx, 0.5, -sz);
+		v[4].set(-sx, 0.5, -sz);
+		v[5].set(-sx, 0.5, sz);
+		
+		var roof_mesh = new THREE.Mesh(roof, new THREE.MeshLambertMaterial());
+		roof_mesh.scale.set(scale.x, scale.y, scale.z);
+
+		var p = this.mesh.position;
+		roof_mesh.position.set(p.x, p.y + scale.y, p.z);
+
+		this.children.push(new Shape(this, roof_mesh, 'roof'));
+
+		//return roof_mesh;
+	};
+
+	// Scale of the space
+	// Width for how much space on the perimeter should the columns take up
+	createBoxwColGeometry(scale, width) {
+		if (!width) {
+			width = 0.25;
+		}
+
+		var sx = (scale.x - width) / scale.x;
+		var sz = (scale.z - width) / scale.z;
+
+		var geo = new THREE.BoxGeometry( sx, 1.0, sz );
+		var v = geo.vertices;
+
+		var r = width / 4.0;
+		
+		var colGeo1 = new THREE.CylinderGeometry(r, r, 1.0, 10.0);
+		colGeo1.scale(1.0/scale.x, 1.0, 1.0/scale.z);
+		colGeo1.translate(sx / 2.0 + r, 0.0, sz / 2.0 + r);
+
+		var colGeo2 = new THREE.CylinderGeometry(r, r, 1.0, 10.0);
+		colGeo2.scale(1.0/scale.x, 1.0, 1.0/scale.z);
+		colGeo2.translate(sx / 2.0 + r, 0.0, -sz / 2.0 - r);
+
+		var colGeo3 = new THREE.CylinderGeometry(r, r, 1.0, 10.0);
+		colGeo3.scale(1.0/scale.x, 1.0, 1.0/scale.z);
+		colGeo3.translate(-sx / 2.0 - r, 0.0, sz / 2.0 + r);
+
+		var colGeo4 = new THREE.CylinderGeometry(r, r, 1.0, 10.0);
+		colGeo4.scale(1.0/scale.x, 1.0, 1.0/scale.z);
+		colGeo4.translate(-sx / 2.0 - r, 0.0, -sz / 2.0 - r);
+
+		geo.merge(colGeo1);
+		geo.merge(colGeo2);
+		geo.merge(colGeo3);
+		geo.merge(colGeo4);
+
+		var mesh = new THREE.Mesh(geo, new THREE.MeshLambertMaterial());
+		mesh.scale.set(scale.x, scale.y, scale.z);
+		return mesh;
+	}
+
 	// THIS IS WHERE ALL THE GRAMMAR IS BASICALLY
 	// This is called by the ShapeSystem whatever.
 	iterate() {
@@ -204,18 +308,36 @@ export default class Shape {
 			}
 		}
 
-		if (this.name == 'building') {
+		// This is a finished intermediate node that no longer matters in terms of iteration
+		if (this.name == '') {
+			return;
+		} else if (this.name == 'building') {
+			// Subdivision into more buildings
 			if (r < 0.8) {
-				// Subdivide
+				if (r < 0.4) {
+					this.subdivide(0);
+				} else {
+					this.subdivide(1);
+				}
 
-				this.subdivide(0);
+			// Divides the building into at least 2 floors
+			// More floors the taller the building is though
 			} else {
-				r = Math.random();
+				this.createFloors(Math.floor(Math.random() * this.mesh.scale.y) + 2);
+			}
+		} else if (this.name == 'top') {
+			if (r  < 0.8) {
+				// make a roof
+				console.log('making a roof');
+				this.createRoofGeometry(this.mesh.scale, this.mesh.scale.x / 4.0);
+			}
+		} else if (this.name == 'bottom') {
+			if (r < 0.5) {
+				// add columns
 
-				this.createFloors(6); // Creates floors
 			}
 		}
-	}
+	};
 
 	draw(scene, n) {
 		// if (n < this.iteration && this.children.length != 0) {
