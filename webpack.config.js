e5cc9bc03ac4fc6c0763fe45c0f3f7c13d74bdfd
Suzanne Knop <sknop8@gmail.com>
2017-02-18 17:18:19 -0500

    Webpack config update

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
   entry: path.join(__dirname, "src/main"),
   output: {
-     path: path.join(__dirname, "build"),
     filename: "./bundle.js"
   },
   module: {
